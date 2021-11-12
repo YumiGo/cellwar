18545441914e64c8220ac0b5bd8b6e4f16712756
doding2 <65176422+doding2@users.noreply.github.com>
2021-11-12 12:53:23 +0900

    Change all the comments to English

--- Cell War/cells.js
+++ Cell War/cells.js
@@ -39,15 +39,14 @@ let up = 87, down = 83; // UP: "W" DOWN: "S"
 
 let cell; // immune cell
 
-var cancerIndex = 0;	// 암세포들의 총 개수(이미 죽은 암세포도 포함)
-var cancerList = [];	// 암세포 변수들을 담아놓는 리스트
+var cancerIndex = 0;	// total number of cancers (including dead cancers)
+var cancerList = [];	// list of cancer cell variables
 
-// 적혈구(먹이)의 총 개수, 적혈구들을 담아놓는 리스트
-var bloodCellIndex = 0;
-var bloodCellList = [];
-var totalNumOfBloodCells = 400;
+var bloodCellIndex = 0;	// total number of blood cells (including dead blood cells)
+var bloodCellList = [];	// list of cancer cell variables
+var totalNumOfBloodCells = 400;	// number of alive blood cells (no dead blood cells)
 
-// 게임 종료시 setInterval 함수의 중단을 위해 필요한 변수 저장용 리스트
+// list for storing variables required to interrupt the setInterval() function at the end of the game
 var intervalVariables = [];
 
 window.onload = function showGameStart(){
@@ -147,33 +146,32 @@ function init(){
 	scene.add(light4);
 
 	window.addEventListener("keydown", keyCodeOn, false);
-	scoreNum.innerHTML = score; // 게임화면에 점수 표시
+	scoreNum.innerHTML = score;	// show score in game dispaly
 
 	loader = new THREE.GLTFLoader(loadingManager);
 
-	// gltf 모델링 불러와서 면역세포(유저) 만들기
+	// load gltf model of immune cell (user)
     loadImmuneCell();
 	
-	// gltf 모델링 불러와서 적혈구(먹이) 만들기
+	// load gltf model of blood cell (food)
 	loadBloodCell();
 
-	// gltf 모델링 불러와서 엄마 암세포(적) 만들기
+	// load gltf model of cancer (enemy)
 	loadRootCancer();
 	
-	// 매 초마다 특정 함수들 실행
-	var interval = setInterval(updateCancersState, 1000);	// 1초마다 포인트, 상태 업데이트
+	var interval = setInterval(updateCancersState, 1000);	// every second, update status and point of cancers
 	intervalVariables.push(interval);
 
-	interval = setInterval(changeCancersDirection, 500);	// 0.5초마다 암세포 이동 방향 조정
+	interval = setInterval(changeCancersDirection, 500);	// every 0.5 seconds, adjust direction of cancers
 	intervalVariables.push(interval);
 
-	interval = setInterval(increaseCancersScale, 100);	// 0.1초마다 크기 증가
+	interval = setInterval(increaseCancersScale, 100);	// every 0.1 seconds, increase scale of cancers
 	intervalVariables.push(interval);
 
-	interval = setInterval(moveCancers, 100);	// 0.1초마다 암세포 이동
+	interval = setInterval(moveCancers, 100);	// every 0.1 seconds, move cancers
 	intervalVariables.push(interval);
 
-	interval = setInterval(detectCollision, 200);	// 0.2초마다 충돌 탐지
+	interval = setInterval(detectCollision, 200);	// every 0.2 seconds, detect collision
 	intervalVariables.push(interval);
 
 	animate();
@@ -196,14 +194,14 @@ function animate() {
 }
 
 
-/**** 면역세포 ****/
-// gltf 파일에서 모델링 불러와서 면역세포(user)를 만드는 함수
+/**** immune cell ****/
+// function that load gltf model of immune cell and create user objet
 function loadImmuneCell() {
 
    loader.load('./lympocyte/scene.gltf', function(gltf) {
        cell = gltf.scene.children[0];
        cell.scale.set(5, 5, 5);
-       cell.position.set(0, 0, 0);    // 물체 위치
+       cell.position.set(0, 0, 0);
 	   cell.point = 0;
        scene.add(cell);
 	   controls.target.set(cell.position.x, cell.position.y, cell.position.z);
@@ -213,7 +211,7 @@ function loadImmuneCell() {
 
 }
 
-// 면역세포(user) 방향 조작 가능케하는 함수
+// unction that enables user controling
 function keyCodeOn(e) {
 	direction = camera.getWorldDirection(vector).multiplyScalar(distance);
 	
@@ -242,8 +240,8 @@ function keyCodeOn(e) {
 }
 
 
-/**** 암세포 ****/
-// gltf 파일에서 모델링 불러와서 맨 처음 모든 암세포의 엄마가 되는 root cancer 만드는 함수
+/**** canecr ****/
+// function that load gltf model of cancer and create root cancer objet
 function loadRootCancer() {
 
 	loader.load('./cancer_model/scene.gltf', function(gltf){
@@ -263,7 +261,7 @@ function loadRootCancer() {
 
 		scene.add(cancer);
 		num_cancer = 1;
-		cancerList[cancerIndex] = cancer;					// 각 cancer들에 접근하기 위해 리스트에 따로 저장
+		cancerList[cancerIndex] = cancer;
 		console.log("Mom " + cancerIndex + " is created [" + cancer.direction + "]")
 		cancerIndex++;
 
@@ -272,16 +270,14 @@ function loadRootCancer() {
 	});
 }
 
-// 암세포가 움직일 방향 또는 엄마 암세포로부터 떨어진 초기 위치(offset)를 생성해서 리턴해주는 함수
+// function that determines the direction in which cancer cells move
 function getRandomOffset() {
 	var offset = [];
 
-	// 총 27가지의 경우의 수
 	for (var i = 0; i < 3; i++) {
-		// 범위가 0 ~ 2인 난수 생성
 		var randomValue = Math.floor(Math.random() * 3);
 
-		// 0, 40, -40 중 하나를 offset에 push
+		// 0, 40, -40
 		if (randomValue === 0)
 			offset.push(0);
 		else if (randomValue === 1) 
@@ -293,10 +289,10 @@ function getRandomOffset() {
 	return offset;
 }
 
- // 엄마 암세포로부터 자식 암세포 생성하는 함수
+ // function of generating baby cancer cells from mom cancer cell
 function createBabyCancer(momIndex, offset) {
 
-	// 엄마 암세포 있나 없나 체크
+	// check to see if there is mom cancer cell
 	var momCancer = cancerList[momIndex]
 	if (!momCancer) 
 		return;
@@ -317,49 +313,50 @@ function createBabyCancer(momIndex, offset) {
 
 	scene.add(babyCancer);
 	num_cancer = num_cancer + 1;
-	cancerList[cancerIndex] = babyCancer;			// 각 cancer들에 접근하기 위해 리스트에 따로 저장
+	cancerList[cancerIndex] = babyCancer;
 	console.log(cancerIndex++ + " is created [" + babyCancer.direction + "]")
 }
 
-// index값에 해당하는 cancer를 scene에서 제거하는 함수
+// Function to remove the scancer corresponding to the index value from the scene.
 function removeCancer(i) {
 
-	// 해당 index에 아무것도 없다면 함수 종료
+	// return this function if there is nothing in the index.
 	var cancer = cancerList[i];
 	if (!cancer)
 		return;
 		
-    scene.remove(cancer);					// scene에서 제거, 더이상 렌더링 되지 않음
+    scene.remove(cancer);				// Removed from the scene, no longer rendered.
 	num_cancer = num_cancer - 1;
 	cancerList[i] = undefined;
 	console.log(cancer.index + " is killed");
 }
 
-// 암세포들의 상태, 포인트를 업데이트해줌. 포인트가 일정 수준에 다다르면 분열
+// function that updates the state and points of cancer cells. Divides when points reach a certain level.
 function updateCancersState() {
 	for (var i = 0; i < cancerIndex; i++) {
 
-		// 해당 index에 아무것도 없다면 이번 루프 패스
 		var cancer = cancerList[i];
 		if (!cancer)
 			continue;
 
-		// 암세포 포인트 업데이트
+		// update point of caner
 		if (cancer.state === GROWING) {
 			cancer.point += 1;
 		}
 
-		// 엄마 암세포의 포인트가 50이 되고 이미 죽은 상태가 아니라면, 애기 암세포 3개 생성 후 제거
+		// If it is not already dead, 
+		// three baby cancer cells are created 
+		// and removed when the point of the mother's cancer cells reaches 50.
 		if (cancer.state !== DEAD && cancer.point === 50) {
 
 			cancer.state = DEAD;
 
-			// 암세포 초기 위치, 방향 값 랜덤 생성
+			// Random generation of the initial location and direction of cancer cells
 			var offset1 = getRandomOffset();
 			var offset2 = getRandomOffset();
 			var offset3 = getRandomOffset();
 
-			// 암세포끼리 겹치는것 방지
+			// prevent cancer cells from overlapping at first
 			while (offset1 === offset2) {
 				offset2 = getRandomOffset();
 			}
@@ -375,23 +372,22 @@ function updateCancersState() {
 		}
 	}
 	if (num_cancer > 15) {
-		over = true; // 게임이 끝났다
-		win = false; //졌다
+		over = true; // game over 
+		win = false; // lose
 	}
 	if (over) {
 		showGameOver();
 	}
 }
 
-// 암세포들의 크기를 증가시키는 함수
+// function that increase scale of cancers
 function increaseCancersScale() {
 	for (var i = 0; i < cancerIndex; i++) {
 
-		// 해당 index에 아무것도 없다면 이번 루프 패스
 		if (!cancerList[i])
 			continue;
 
-		// 암세포 상태 업데이트
+		// increase scale of cancer
 		var cancer = cancerList[i];
 		if (cancer.state === GROWING) {
 			cancer.scale.set(cancer.scale.x + 0.05, cancer.scale.y + 0.05, cancer.scale.z + 0.05);
@@ -399,22 +395,21 @@ function increaseCancersScale() {
 	}
 }
 
-// 암세포들을 자동으로 움직이게 하는 함수
+// function that automatically moves cancer cells
 function moveCancers() {
 	for (var i = 0; i < cancerIndex; i++) {
 
-		// 해당 index에 아무것도 없다면 이번 루프 패스
 		var cancer = cancerList[i];
 		if (!cancer)
 			continue;
 
-		// 암세포 위치 이동
+		// move cancer
 		if (cancer.state === GROWING || cancer.state === ADJUSTING) {
 			cancer.position.x += cancer.direction[0] / 30;
 			cancer.position.y += cancer.direction[1] / 30;
 			cancer.position.z += cancer.direction[2] / 30;
 
-			// 암세포가 맵 밖으로 나갔을 경우
+			// When cancer cells go out of the map
 			if (cancer.state !== ADJUSTING &&
 				(cancer.position.x >= maxMapSize || cancer.position.x <= minMapSize ||
 				cancer.position.y >= maxMapSize || cancer.position.y <= minMapSize ||
@@ -425,8 +420,6 @@ function moveCancers() {
 					cancer.direction[0] = cancer.direction[1]
 					cancer.direction[1] = cancer.direction[2]
 					cancer.direction[2] = temp;
-					console.log("암세포 맵 이탈");
-					console.log(cancer.position);
 				}
 			else if (cancer.state === ADJUSTING && 
 				cancer.position.x <= maxMapSize && cancer.position.x >= minMapSize &&
@@ -438,16 +431,15 @@ function moveCancers() {
 	}
 }
 
-// 암세포들이 움직이는 방향을 주기적으로 조정해주는 함수
+// function that periodically adjusts the direction in which cancer cells move
 function changeCancersDirection() {
 	for (var i = 0; i < cancerIndex; i++) {
 
-		// 해당 index에 아무것도 없다면 이번 루프 패스
 		var cancer = cancerList[i];
 		if (!cancer)
 			continue;
 
-		// 암세포 이동 방향 조정
+		// adjust direction of cacners
 		if (cancer.state === GROWING) {
 			cancer.direction[0] += cancer.direction[1] / 20;
 			cancer.direction[1] += cancer.direction[2] / 100;
@@ -461,40 +453,40 @@ function changeCancersDirection() {
 	}
 }
 
-// 면역세포-적혈구, 면역세포-암세포간의 충돌 탐지
+// detection of collision between immune cells - red blood cells and immune cells - cancer cells
 function detectCollision() {
 	var momBloodCell = bloodCellList[0];
 	if (!momBloodCell)
 		return;
 
-	// 면역세포와 적혈구의 충돌 탐지
+	// collision between immune cells - red blood cells
 	for (var i = 1; i < bloodCellIndex; i++) {
 		var bloodCell = bloodCellList[i];
 		if (!bloodCell)
 			continue;
 
-		// 충돌했을 때
+		// when it collides
 		if (getCollision(cell, bloodCell)) {
-			// 먹힌 적혈구 제거
+			// remove the eaten red blood cell
 			scene.remove(bloodCell);
 			bloodCellList[i] = undefined;
-			// 면역세포 포인트 증가 (10만큼)
+			// increased immune cell points (by 10)
 			cell.point += bloodCell.point;
 			cell.scale.set(cell.scale.x + 1.0, cell.scale.y + 1.0, cell.scale.z + 1.0);
-			console.log("적혈구와의 충돌 발생, 점수: " + cell.point);
+			console.log("point: " + cell.point);
 
-			// 새로운 적혈구 하나 생성
+			// create a new red blood cell
 			createRandomBloodCell(momBloodCell);
 		}
 	}
 
-	// 면역세포와 암세포의 충돌 탐지
+	// collision between immune cells - cancers
 	for (var i = 0; i < cancerIndex; i++) {
 		var cancer = cancerList[i];
 		if (!cancer)
 			continue;
 
-		// 충돌했을 때
+		// when it collides
 		if (getCollision(cell, cancer)) {
 			var cellDecrease = cancer.point * 0.1;
 			var cancerDecrease = cell.point * 0.1;
@@ -502,22 +494,22 @@ function detectCollision() {
 			cell.scale.set(cell.scale.x - cellDecrease, cell.scale.y - cellDecrease, cell.scale.z - cellDecrease);
 			cancer.scale.set(cell.scale.x - cancerDecrease, cell.scale.y - cancerDecrease, cell.scale.z - cancerDecrease);
 			
-			// 암세포의 포인트만큼 제거
+			// Decrease the points of immune cells as much as the points of cancer cells.
 			var temp = cancer.point;
 			cancer.point -= cell.point;
 			cell.point -= temp;
 
-			console.log("암세포와의 충돌 발생, 점수: " + cell.point);
+			console.log("point: " + cell.point);
 			if (cell.point <= 0) {
-				// 게임 오버
-				console.log("면역세포의 포인트가 0 이하가 되었으므로 게임 오버");
+				// game over
+				console.log("Game over because the point of the immune cell is below zero.");
 				stop();
 				showGameOver();
 
 				return;
 			}
 
-			if (cancer.point <= 0) {	// 암세포 죽음			
+			if (cancer.point <= 0) {	// cancer cell death		
 				score += 10;	// 10 points per one cancer cell
 				scoreNum.innerHTML = score;		// update score
 
@@ -525,9 +517,9 @@ function detectCollision() {
 				num_cancer = num_cancer - 1;
 				cancerList[i] = undefined;
 
-				// 두 가지 win 조건을 체크함
+				// Checked the two win conditions
 				if(score == 50)	{
-					console.log("점수가 50이 되었으므로 게임 오버");
+					console.log("Game over because the score is 50.");
 
 					over = true; 
 					win = true;
@@ -538,7 +530,7 @@ function detectCollision() {
 				}
 				
 				if (num_cancer  == 0) {
-					console.log("화면에 있는 암세포를 모두 제거하였으므로 게임 오버");
+					console.log("Game over because all cancer cells on the screen have been removed.");
 
 					over = true; 
 					win = true;
@@ -553,28 +545,28 @@ function detectCollision() {
 	}
 }
 
-// 인자로 넘겨진 두 object가 충돌했는지 안했는지 판단하는 함수
+// function to determine whether the two objects passed collided or not.
 function getCollision(first, second) {
 	if (!first || !second)
 		return;
 
-	// first 현재 좌표
+	// coordinates of first object
 	var cp = [first.position.x, first.position.y, first.position.z];
 
-	// first 반지름
+	// radius of first object
 	var firstBox = new THREE.Box3().setFromObject( first );
 	var firstRadius = Math.floor(Math.abs(firstBox.max.x - firstBox.min.x)) / 2;
 
-	// obj2 현재 좌표
+	// coordinates of second object
 	var np = [second.position.x, second.position.y, second.position.z];
-	// obj2 반지름
+	// radius of second object
 	var secondBox = new THREE.Box3().setFromObject( second );
 	var secondRadius = Math.floor(Math.abs(secondBox.max.x - secondBox.min.x)) / 2;
 
-	// cancer와 next_cancer간의 거리
+	// The distance between first object and second object
 	var distance = Math.floor(Math.sqrt(Math.pow(np[0] - cp[0], 2) + Math.pow(np[1] - cp[1], 2) + Math.pow(np[2] - cp[2], 2)));
 	
-	// 충돌 발생
+	// Collision occurred
 	if (distance <= firstRadius + secondRadius) {
 		return true;
 	}
@@ -584,8 +576,8 @@ function getCollision(first, second) {
 }
 
 
-/**** 적혈구(먹이) ***/
-// gltf 파일에서 적혈구 모델링을 불러오는 함수
+/**** Blood Cell (food) ***/
+// function that load gltf model of blood cell and create blood cell objet
 function loadBloodCell() {
 	loader.load('./blood_cell_model/scene.gltf', function(gltf){
 		var cell = gltf.scene.children[0];
@@ -604,7 +596,8 @@ function loadBloodCell() {
 	});
 }
 
-// 적혈구 100개를 복사해서 랜덤한 곳에 뿌려주는 함수
+// function of copying red blood cells as many as totalNumOfBloodCells 
+// and spraying them in random places
 function copyAndSpreadBloodCells() {
 	var bloodCell = bloodCellList[0];
 	if (!bloodCell)
@@ -635,7 +628,7 @@ function createRandomBloodCell(momBloodCell) {
 	bloodCellIndex++;
 }
 
-// 범위가 minMapSize ~ maxMapSize인 난수를 생성하는 함수 (적혈구 좌표 랜덤 생성용)
+// function that generates random numbers with a range of minMapSize to maxMapSize
 function getRandomValue() {
 	return Math.floor(Math.random() * (maxMapSize - minMapSize) + minMapSize);
 }
